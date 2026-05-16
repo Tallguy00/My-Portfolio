4bdb63cb07b400bcb228a19c3da389189cfa0627
Alazar <alazarwendater@gmail.com>
2026-05-16 04:35:18 +0300

    Fix the fav icon

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -27,7 +27,7 @@ export default function Projects() {
       id: 3,
       title: 'Haramaya Health Risk Prediction System',
       description: 'It combines modern information technology with healthcare practices to create a smarter and more reliable health monitoring environment.',
-      image: '../../public/image.png',
+      image: '/image.png',
       tags: ['TypeScript', 'React', 'Tailwind', 'Python'],
       liveUrl: 'https://haramaya-health-risk-ai.vercel.app/',
       githubUrl: 'https://github.com/Tallguy00/Haramaya-Health-Risk-AI',
